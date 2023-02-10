85f90a560768ba7ef2c5d0a3922e33bc04908429
kennethgallo <61844301+kennethgallo@users.noreply.github.com>
2023-02-09 21:31:47 -0500

    added background music

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.init()
 display_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 WINDOW_WIDTH, WINDOW_HEIGHT = pygame.display.get_window_size()
 
+# Music by Kim Lightyear from Pixaby
+pygame.mixer.music.load('Music/bg-song.mp3')
+pygame.mixer.music.play(-1, 0.0)
+
 # Main game loop
 running = True
 while running:
