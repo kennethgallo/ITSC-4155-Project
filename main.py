51b1f5dc8d26d9f17b8dbd7557755724ec2fac14
MatthewCarroll2001 <matchewcarasel@gmail.com>
2023-02-10 16:39:44 -0500

    moved player to a sprite class in a separate file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from Player import Player
 
 # Initialize pygame
 pygame.init()
@@ -13,11 +14,8 @@ pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.01)
 
 # Player image, coordinates, and speed
-player = pygame.image.load('Assets/player.png')
-player = pygame.transform.scale(player, (60, 60))
-player_x = 0
-player_y = 0
-speed = 2
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 
 # Make background surface
 background_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -30,20 +28,14 @@ while running:
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
-    # player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_y -= speed
-    if keys[pygame.K_s]:
-        player_y += speed
-    if keys[pygame.K_a]:
-        player_x -= speed
-    if keys[pygame.K_d]:
-        player_x += speed
-
+    # Update surfaces
     display_surface.blit(background_surf, (0, 0))
-    display_surface.blit(player, (player_x, player_y))
 
+    # Update player
+    player.draw(display_surface)
+    player.update()
+
+    # Update display
     pygame.display.update()
 
 # End the game
