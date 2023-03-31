1d4d08f51814ce38fbde44ad2c6d7ab8a7bf0119
MatthewCarroll2001 <matchewcarasel@gmail.com>
2023-03-31 14:32:59 -0400

    moved enemy sprite and spawn logic to enemy_spawn.py

--- enemy_spawn.py
+++ enemy_spawn.py
@@ -1,4 +1,5 @@
-
+import pygame
+from enemy import Enemy
 
 enemy_spawn_points = [
     (100, 100),
@@ -9,3 +10,20 @@ enemy_spawn_points = [
     (400, 650),
     (300, 50)
 ]
+
+class EnemySpawner():
+    def __init__(self, display_surface, player):
+        self.display_surface = display_surface
+        self.player = player
+        self.round = 1
+        self.delay_between_enemies = 1000
+        self.enemy_sprite_group = pygame.sprite.Group()
+        for index in range(len(enemy_spawn_points)):
+            x_pos = enemy_spawn_points[index][0]
+            y_pos = enemy_spawn_points[index][1]
+            enemy = Enemy(index, 100 * self.round, x_pos, y_pos)
+            self.enemy_sprite_group.add(enemy)
+
+    def update(self):
+        self.enemy_sprite_group.draw(self.display_surface)
+        self.enemy_sprite_group.update(self.player, self.enemy_sprite_group)
